d1e27174fd3f3f4cd9b5bdaa4b3b7120f8f1c639
Mohammed Sadiq <mohammedsadiq7499@gmail.com>
2020-08-14 15:57:52 +0530

    frequency of a character in a string

---
+++ freq_of_a_char.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    int i;
+    int freq[256]={0};
+
+    cin>>str;
+    for(i=0;str[i]!='\0';i++)
+    {
+        freq[str[i]]++;
+    }
+    for(i=0;i<256;i++)
+    {
+        if(freq[i]!=0)
+        {
+            cout<<(char)i<<" = "<<freq[i]<<"\n";
+        }
+    }
+    return 0;
+}
